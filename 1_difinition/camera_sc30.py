27f031f3ae9c6a2ac82ae2331c0dde508a6990b5
phoby2007 <yushioi0816@gmail.com>
2026-08-12 20:46:15 +0900

    add camerasc-30.py

--- 1_difinition/camera_sc30.py
+++ 1_difinition/camera_sc30.py
@@ -83,20 +83,12 @@ if __name__ == "__main__":
     while True:
         try:
             cap = camera.capture_image()
-            capcp = cap.copy()
             if cap is None:
                 print("Failed to capture image. Exiting.")
                 break
             cap = camera.histogram_equalization(cap)
             cap = camera.detect_cone(cap)
             cx, cy, cap, camera_order = camera.get_cone_position(cap)
-            if cx is not None and cy is not None:
-                capcp = cv2.circle(capcp, (cx, cy), 10, (0, 255, 0), -1)
-            cv2.imshow("Detected Cone", capcp)
-            try:
-                cv2.imshow("Mask", cap)
-            except cv2.error as e:
-                print(f"Error displaying image: {e}")
             cv2.waitKey(1)  # Adjust the wait time as needed
             print(cx, cy, camera_order)
         except Exception as e:

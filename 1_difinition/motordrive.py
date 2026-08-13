16a48f589b3861c218e3f2e8c0c141ddbe52319e
phoby2007 <yushioi0816@gmail.com>
2026-08-13 10:44:11 +0900

    added inverted fix motordrive.py

--- 1_difinition/motordrive.py
+++ 1_difinition/motordrive.py
@@ -212,6 +212,7 @@ def move(direction, power, duration, is_inverted=False, enable_stack_check=True)
                 for _ in range(5):
                     gyro = bno.gyroscope()
                     accelz = bno.acceleration()[2]  # Z軸加速度を取得
+                    print(f"Gyro: {gyro}, AccelZ: {accelz}")  # デバッグ用 - センサー値を表示
                     # センサーエラー時は検知しない
                     if gyro is None or accelz is None:
                         stack_detected = False

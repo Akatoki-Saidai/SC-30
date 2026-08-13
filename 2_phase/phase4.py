53df8d9061c618d48cdbbf0ee2dca252c77999e2
phoby2007 <yushioi0816@gmail.com>
2026-08-13 12:11:44 +0900

    added inverted fix motordrive.py

--- 2_phase/phase4.py
+++ 2_phase/phase4.py
@@ -187,13 +187,13 @@ def main():
                                 elif order == 2:
                                     print("ターゲットが右です。右に旋回してから前進します。")
                                     if motor_ok:
-                                        md.move('d', power=0.7, duration=0.5, is_inverted=is_inverted, enable_stack_check=False)
+                                        md.move('e', power=0.7, duration=0.5, is_inverted=is_inverted, enable_stack_check=False)
                                         is_stacked = md.move('w', power=0.7, duration=2.0, is_inverted=is_inverted, enable_stack_check=True)
                                         
                                 elif order == 3:
                                     print("ターゲットが左です。左に旋回してから前進します。")
                                     if motor_ok:
-                                        md.move('a', power=0.7, duration=0.5, is_inverted=is_inverted, enable_stack_check=False)
+                                        md.move('q', power=0.7, duration=0.5, is_inverted=is_inverted, enable_stack_check=False)
                                         is_stacked = md.move('w', power=0.7, duration=2.0, is_inverted=is_inverted, enable_stack_check=True)
     
                                 # ④ スタック判定とリカバリー（motordriveにお任せ）

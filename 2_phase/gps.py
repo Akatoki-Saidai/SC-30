5bac109c93aef0efcd06d4edff319d8cb3e1bbe7
phoby2007 <yushioi0816@gmail.com>
2026-08-14 02:56:51 +0900

    fix gps.py

--- 2_phase/gps.py
+++ 2_phase/gps.py
@@ -230,7 +230,7 @@ def calculate_distance_and_angle(current_lat, current_lon, start_lat, start_lon,
 
         # 相対角度（EM.py互換：左+、右-）
         diff_deg = -(az_goal - az_move)
-        print(f"diff : {diff_deg}")
+        print(f"dist_move : {dist_move}, diff : {diff_deg}")
         # -180 ~ 180 に正規化
         while diff_deg > 180:
             diff_deg -= 360
